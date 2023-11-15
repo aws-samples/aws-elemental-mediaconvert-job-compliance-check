02cc9c5aa6dcb07d10c74f0c5441a5c1ee010eb9
Jaiganesh Girinathan <ganeshji@amazon.com>
2023-11-15 15:49:54 -0500

    added cdk.json file

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 export AWS_PROFILE="default"
-export DEPLOY_AWS_REGIONS=('us-east-1')
+export DEPLOY_AWS_REGIONS=('us-east-1' 'us-east-2')
 npm install
 
-npx cdk synth 
+# npx cdk synth 
 
 for region in "${DEPLOY_AWS_REGIONS[@]}"
 do
   export CDK_DEPLOY_REGION=$region
-  npx cdk bootstrap --profile $AWS_PROFILE
-  npx cdk synth --profile $AWS_PROFILE
-  # npx cdk deploy --profile $AWS_PROFILE
+  # npx cdk bootstrap --profile $AWS_PROFILE
+  # npx cdk synth --profile $AWS_PROFILE
+  npx cdk deploy --profile $AWS_PROFILE
 done
